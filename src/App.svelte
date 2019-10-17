c07d2210526a0ec69f10097827e39e764bc6681d
ThomasAndrewMacLean <thomas.maclean@marlon.be>
2019-10-17 13:11:20 +0200

    setup svelte

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let namePiano;
+</script>
+
+<style>
+	
+</style>
+
+<div class="container">
+        <header>
+            <h2>
+                Rent the {namePiano}
+            </h2>
+        </header>
+
+        <main>
+            <p>Avoid unnecessary risk and let Quatre Mains handle transportation of your precious piano. With utmost
+                care we will transport your piano and if you wish tune it at destination. </p>
+
+            <form id="form">
+                <label for="mappp">Type in your address <span>*</span></label>
+                <input name="dream" type="text" aria-labelledby="calculate-button" id="mappp" required />
+                <div class="button-wrap">
+                    <button type="submit" id="calculate-button">Calculate</button>
+                    <span id="result">150€</span>
+                </div>
+            </form>
+
+
+        </main>
+    </div>
