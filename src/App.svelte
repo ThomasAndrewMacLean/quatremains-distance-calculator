24ec68f4b5314ab14a41bc3f3ca2139404cea7dc
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-10-19 21:03:07 +0200

    setup form

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,257 @@
 <script>
-	export let namePiano;
+  import { onMount } from "svelte";
+  import { baseURL } from "./main.js";
+
+  console.log(baseURL);
+  export let namePiano;
+  export let labels = {};
+  let formComplete = false;
+  let partOneOfFormComplete = false;
+  let firstname = "";
+  let lastname = "";
+  let email = "";
+  let date;
+  let address = "";
+
+  let autoComplete;
+  let autoCompleteInput;
+  let submitButton;
+
+  let dateReserved = [];
+
+  fetch(baseURL + "/available/" + namePiano)
+    .then(x => x.json())
+    .then(data => (dateReserved = data));
+
+  onMount(() => {
+    autoComplete = new google.maps.places.Autocomplete(autoCompleteInput, {});
+  });
+  let submit = e => {
+    e.preventDefault();
+    if (dateReserved.includes(date)) return;
+
+    submitButton.disabled = true;
+
+    if (!partOneOfFormComplete) {
+      fetch(baseURL + "/getDistance", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          firstname,
+          piano: namePiano,
+          lastname,
+          email,
+          date,
+          address
+        })
+      })
+        .then(x => {
+          if (x.status === 200) {
+            submitButton.disabled = false;
+
+            partOneOfFormComplete = true;
+          }
+        })
+        .catch(err => {
+          throw new Error(err);
+        });
+    } else {
+      fetch(baseURL + "/rent", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          firstname,
+          piano: namePiano,
+          lastname,
+          email,
+          date,
+          address
+        })
+      })
+        .then(x => {
+          if (x.status === 200) {
+            submitButton.disabled = false;
+            formComplete = true;
+          }
+        })
+        .catch(err => {
+          throw new Error(err);
+        });
+    }
+  };
 </script>
 
 <style>
-	
+  .input-row {
+    display: flex;
+    justify-content: space-between;
+  }
+  .input-wrap {
+    width: 47%;
+  }
+  .input-row > .input-wrap:first-of-type {
+    margin-right: 6%;
+  }
+  .text-area-wrap {
+    width: 100%;
+  }
+  textarea {
+    width: 100%;
+    height: 150px;
+  }
+
+  h4 {
+    text-transform: uppercase;
+    color: var(--colour-gold);
+    font-family: "Open Sans", sans-serif;
+    font-weight: 100;
+  }
+
+  .warning {
+    background: lightcoral;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
+  }
+
+  .date-input {
+    position: relative;
+  }
 </style>
 
 <div class="container">
-        <header>
-            <h2>
-                Rent the {namePiano}
-            </h2>
-        </header>
+  <header>
+    <h2>{labels.title || 'Rent the'} {namePiano.replace('-', ' ')}</h2>
+    <h4>
+      {labels.subtitle || 'FILL IN THIS FORM TO GET AN APPROXIMATION OF THE PRICE'}
+    </h4>
+  </header>
 
-        <main>
-            <p>Avoid unnecessary risk and let Quatre Mains handle transportation of your precious piano. With utmost
-                care we will transport your piano and if you wish tune it at destination. </p>
+  <main>
+    <p>
+      {labels.intro || `Avoid unnecessary risk and let Quatre Mains handle transportation of your
+      precious piano. With utmost care we will transport your piano and if you
+      wish tune it at destination.`}
+    </p>
 
-            <form id="form">
-                <label for="mappp">Type in your address <span>*</span></label>
-                <input name="dream" type="text" aria-labelledby="calculate-button" id="mappp" required />
-                <div class="button-wrap">
-                    <button type="submit" id="calculate-button">Calculate</button>
-                    <span id="result">150€</span>
-                </div>
-            </form>
+    <form on:submit={submit}>
+      <div class="input-row">
+        <div class="input-wrap">
+          <label for="firstname">
+            {labels.firstName || 'First name'}
+            <span>*</span>
+          </label>
+          <input
+            bind:value={firstname}
+            name="firstname"
+            type="text"
+            id="firstname"
+            required />
+        </div>
 
+        <div class="input-wrap">
+          <label for="lastname">
+            {labels.lastName || 'Last name'}
+            <span>*</span>
+          </label>
+          <input
+            bind:value={lastname}
+            name="lastname"
+            type="text"
+            id="lastname"
+            required />
+        </div>
+      </div>
 
-        </main>
-    </div>
+      <div class="input-row">
+        <div class="input-wrap date-input">
+          <label for="date">
+            {labels.date || 'Date'}
+            <span>*</span>
+          </label>
+          <input bind:value={date} name="date" type="date" id="date" required />
+          {#if dateReserved.includes(date)}
+            <span class="warning">Piano not available on this date</span>
+          {/if}
+        </div>
+
+        <div class="input-wrap">
+          <label for="email">
+            {labels.email || 'Email'}
+            <span>*</span>
+          </label>
+          <input
+            bind:value={email}
+            name="email"
+            type="email"
+            id="email"
+            required />
+        </div>
+      </div>
+
+      <label for="address">
+        {labels.address || 'Type in your address'}
+        <span>*</span>
+      </label>
+      <input
+        bind:this={autoCompleteInput}
+        bind:value={address}
+        name="address"
+        type="text"
+        id="address"
+        required />
+
+      {#if partOneOfFormComplete}
+        <h4>
+          {labels.fillInDetails || 'Fill in the rest of these details to rent the instrument.'}
+        </h4>
+
+        <div class="input-row">
+          <div class="input-wrap">
+            <label for="telephone">
+              {labels.telephone || 'Telephone'}
+              <span>*</span>
+            </label>
+            <input name="telephone" type="telephone" id="telephone" required />
+          </div>
+
+        </div>
+
+        <div class="input-row">
+          <div class="text-area-wrap">
+            <label for="message">
+              {labels.message || 'Message'}
+              <span>*</span>
+            </label>
+            <textarea id="message" name="message" />
+          </div>
+
+        </div>
+      {/if}
+
+      <div class="button-wrap">
+        <button
+          bind:this={submitButton}
+          class={dateReserved.includes(date) ? 'disabled' : ''}
+          type="submit"
+          id="calculate-button">
+          {partOneOfFormComplete ? labels.rent || 'Rent' : labels.calculate || 'Calculate'}
+        </button>
+        {#if partOneOfFormComplete}
+          <button type="button" class="secondary-btn">
+            {labels.cancel || 'Cancel'}
+          </button>
+        {/if}
+      </div>
+    </form>
+
+  </main>
+</div>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
-import App from './App.svelte';
+// polyfills
+import 'whatwg-fetch'
+import 'url-search-params-polyfill'
 
-const app = new App({
-	target: document.body,
-	props: {
-		namePiano: 'G4 Harpsicord'
-	}
-});
+import App from './App.svelte'
 
-export default app;
+export const baseURL = 'http://localhost:3000'
+
+const searchParams = new URLSearchParams(window.location.search)
+const namePiano = (searchParams.get('piano') || 'yamaha-cfx')
+const language = searchParams.get('language')
+fetch(baseURL + '/labels/' + language)
+    .then(x => x.json())
+    .then(labels => {
+        new App({
+            target: document.body,
+            props: {
+                namePiano,
+                labels,
+            },
+        })
+    })
+    .catch(err => {
+        // eslint-disable-next-line no-console
+        console.error(err)
+        throw Error(err)
+    })
